36a069713713befaf6012e63603ed60f40f13cce
manfred gruenling <manfred.gruenling@gmail.com>
2014-09-25 01:56:45 +0200

    user-input detection on server side, including some security checks user-actions are stored in movement-object

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
-var express, app, server, io, game;
+var express, app, server, io, websocket;
 
 express = require('express');
 app = express();
 server = require('http').createServer(app);
 io = require('socket.io').listen(server);
-game = require('./game.js');
+websocket = require("websocket");
 
-
-// Create Webserver
+// create web-server
 server.listen(3000);
 console.log("Server is listening on http://localhost:3000...");
 app.get('/', function(req, res){
 	res.sendfile(__dirname +'/index.html');
 });
 
-// Create Websocket-Connections
-var numberOfPushConnections = 0;
-io.sockets.on('connection', function(socket){
 
-    numberOfPushConnections++;
-
-    console.log("A client is now connected", numberOfPushConnections);
-
-    socket.on('player-input', function(data){
-        io.sockets.emit('update-scene', data.name+"a");
-    });
-
-    socket.on('move', function(data){
-        game.move();
-        console.log(data);
-    });
-
-    socket.on('disconnect', function () {
-        numberOfPushConnections--;
-        console.log("A client has closed the connection.", numberOfPushConnections);
-    });
-
-});
+// Listen for websocket-connections
+websocket.start(io);
 
