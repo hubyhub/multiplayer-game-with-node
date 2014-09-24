00fb6bf2c343f99f2bd7058bfc05586c14b3eb7a
manfred gruenling <manfred.gruenling@gmail.com>
2014-09-24 23:04:11 +0200

    small changes on node.js side

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
+var express, app, server, io, game;
 
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
-    game = require('./game.js');
+express = require('express');
+app = express();
+server = require('http').createServer(app);
+io = require('socket.io').listen(server);
+game = require('./game.js');
 
 
-var numberOfConnections = 0;
+// Create Webserver
 server.listen(3000);
 console.log("Server is listening on http://localhost:3000...");
-
 app.get('/', function(req, res){
 	res.sendfile(__dirname +'/index.html');
 });
 
+// Create Websocket-Connections
+var numberOfPushConnections = 0;
 io.sockets.on('connection', function(socket){
-    numberOfConnections++;
-    console.log("A client is now connected", numberOfConnections);
 
+    numberOfPushConnections++;
+
+    console.log("A client is now connected", numberOfPushConnections);
 
     socket.on('player-input', function(data){
-
         io.sockets.emit('update-scene', data.name+"a");
-
-        //io.broadcast.emit('update-players',data.name+"a");
     });
 
     socket.on('move', function(data){
@@ -31,12 +31,10 @@ io.sockets.on('connection', function(socket){
         console.log(data);
     });
 
-
     socket.on('disconnect', function () {
-        numberOfConnections--;
-        console.log("A client has closed the connection.", numberOfConnections);
+        numberOfPushConnections--;
+        console.log("A client has closed the connection.", numberOfPushConnections);
     });
 
 });
 
-//http://socket.io/docs/#using-with-node-http-server
